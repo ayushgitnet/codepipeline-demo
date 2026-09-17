54f1adde0b6a1f07468d7493681539687bdeb583
Ayush Gupta <192564024+ayushgitnet@users.noreply.github.com>
2026-09-17 09:46:12 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,12 +2,5 @@
 systemctl restart httpd
 •	Commit/push all files to GitHub.
 •	The script must be inside the scripts folder.
-8. Prepare EC2 Web Server
-•	Launch Amazon Linux 2023 EC2.
-•	Security Group: allow HTTP 80 and SSH 22 for the lab.
-sudo dnf update -y
-sudo dnf install -y httpd
-sudo systemctl enable httpd
-sudo systemctl start httpd
-sudo systemctl status httpd
+
 
